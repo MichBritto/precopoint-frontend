0e0ae3f300320cf4de9b8083b6c471ad28556e59
Michel Brito <michel.brito.9699@gmail.com>
2023-05-29 10:27:58 -0300

    implementado permissionamento ao acessar rotas

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,30 @@
+import router from '@/router';
 import axios , {AxiosInstance} from 'axios'
+import Cookies from 'js-cookie'
+import Swal from 'sweetalert2'
 
 const api: AxiosInstance = axios.create({
     baseURL: "http://localhost:8080/"
 });
 
+api.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        if(error.response.status === 403) {
+            //erro 403 = o acesso foi negado
+            Cookies.remove('token');
+            Cookies.remove('email');
+            router.push('/login');
+            Swal.fire({
+                title: 'Psiuuu, espera aí...',
+                text: 'Antes de prosseguir para a página você precisa fazer login!',
+                icon: 'warning',
+            })
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default api;
